b59ade1be12f797798d35f7d86d1d378cab78373
Gustavo Claudino Kresko <gustavock@whbbrasil.com.br>
2025-05-23 16:19:15 -0300

    Ajuste conexao com o front, Net 8.0, Conexao do back com o banco

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,37 +3,41 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirTudo", policy =>
     {
-        policy.AllowAnyOrigin()      // Permite qualquer origem
-              .AllowAnyMethod()      // Permite qualquer método HTTP
-              .AllowAnyHeader();     // Permite qualquer cabeçalho
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
 
-// Configuração de serviços
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configure o serviço do DbContext para usar MySQL
+// Banco de Dados
 builder.Services.AddDbContext<Banco>(options =>
-    options.UseMySql("server=localhost;port=3306;database=planner;user=root;password=root",    
-    new MySqlServerVersion(new Version(8, 0, 33))));
+    options.UseMySql(
+        "server=localhost;port=3306;database=planner;user=root;password=root",
+        new MySqlServerVersion(new Version(8, 0, 33))
+    )
+);
 
 var app = builder.Build();
 
-// Rotas e APIs
+// Middleware
+app.UseCors("PermitirTudo");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Rotas
 app.MapGet("/", () => "Loja da UP");
 app.MapLivroApi();
 app.MapPedidoApi();
 app.MapAutorApi();
 app.MapPagamentoApi();
-
-// Swagger
-app.UseSwagger();
-app.UseSwaggerUI();
-
 
 app.Run();
--- backend/models/Pagamentos.cs
+++ backend/models/Pagamentos.cs
@@ -1,7 +1,7 @@
 public class Pagamentos {
     public int Id { get; set;}
     public bool Cartao {get; set;}
-    public bool Pix {get; set}
+    public bool Pix {get; set;}
     public bool Dinheiro {get; set;}
 
 }
